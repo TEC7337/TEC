101da5aaa9ac560c4574ac1e4b9877585bd6a343
TEC7337 <trisch7337@gmail.com>
2025-06-30 13:28:49 -0700

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,24 @@
+def add(x,y):
+	return x+y
+
+def subtract(x,y):
+	return x-y
+
+def multiply(x,y):
+	return x*y
+
+def divide(x,y):
+	return x/y
+
+def power(x,y):
+	return x**y
+
+def mod(x,y):
+	return x%y
+
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
+print(power(1,2))
+print(mod(1,2))
